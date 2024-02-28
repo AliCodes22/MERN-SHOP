4e8e04e365aaa0e0f5b73b5d511804ca2fb18df1
Ali Ahmed <alitine94@gmail.com>
2024-02-27 21:43:38 -0500

    db, models

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
