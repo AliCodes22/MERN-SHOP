74fd23f45a731cf7533a94e89f6a18620cf11aed
Ali Ahmed <alitine94@gmail.com>
2024-02-28 23:48:28 -0500

    seeded dummy data

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 const port = process.env.PORT || 5000;
 
+dotenv.config();
 connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => {
