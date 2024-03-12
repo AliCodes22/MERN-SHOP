a8c1fd919f7494b4251c85d9ff0b8ba3f705514b
Ali Ahmed <alitine94@gmail.com>
2024-03-11 21:17:46 -0400

    payment page done

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -20,6 +20,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ShippingPage from "./pages/ShippingPage";
 import PrivateRoute from "./components/PrivateRoute";
+import PaymentPage from "./pages/PaymentPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
 
       <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
       </Route>
     </Route>
   )
