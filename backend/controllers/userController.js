fe217f15758a05b8f89b6b97a6117edac3914b35
Ali Ahmed <alitine94@gmail.com>
2024-03-20 22:41:20 -0400

    update users controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -110,19 +110,53 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 });
 
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("update user");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.status(201).json({ ...updatedUser, _id: updatedUser._id });
+  } else {
+    res.status(400);
+    throw new Error("user not found");
+  }
 });
 
 const getAllUsers = asyncHandler(async (req, res) => {
-  res.send("get all users");
+  const users = await User.find({});
+
+  res.status(200).json(users);
 });
 
 const getUserById = asyncHandler(async (req, res) => {
-  res.send("get user by id");
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("delete user");
+  const user = await User.find(req.params.id);
+
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error("Cannot delete admin");
+    }
+    await User.deleteOne({ _id: user._id });
+    res.status(201).json({ message: "user deleted" });
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
 });
 
 export {
